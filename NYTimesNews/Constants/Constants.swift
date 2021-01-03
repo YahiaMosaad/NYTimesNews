3fee1dc42cc44da8acefb397c033a57dff0a576a
Yahia Mosaad <yahia.mosaad@gmail.com>
2021-01-03 18:54:20 +0400

    Add constant File

---
+++ NYTimesNews/Constants/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  NYTimesNews
+//
+//  Created by Yahia Mosaad on 03/01/2021.
+//
+
+import Foundation
+struct NetworkConstants {
+    // MARK: - mark APIRequest Constants
+    static let NYTimesAPIBaseURL = "http://api.nytimes.com/svc/mostpopular/v2/"
+}
+
+struct StringConstants {
+    // MARK: - mark Srting Constants
+    static let noInternetMessage = "No Internet Connection"
+    static let noDataMessage = "NO DATA FOUND"
+    static let alertTitle = "Alert"
+    static let okTitle = "Ok"
+    static let errorTitle = "Error"
+    static let cancelTitle = "Cancel"
+}
