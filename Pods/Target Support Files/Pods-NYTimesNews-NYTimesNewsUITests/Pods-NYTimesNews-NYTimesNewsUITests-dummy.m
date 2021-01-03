69fc66951b833440d00e4a3093f29cbc188fb897
Yahia Mosaad <yahia.mosaad@gmail.com>
2021-01-03 12:55:10 +0400

    install pods

---
+++ Pods/Target Support Files/Pods-NYTimesNews-NYTimesNewsUITests/Pods-NYTimesNews-NYTimesNewsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NYTimesNews_NYTimesNewsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_NYTimesNews_NYTimesNewsUITests
+@end
---
+++ Pods/Target Support Files/Pods-NYTimesNewsTests/Pods-NYTimesNewsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NYTimesNewsTests : NSObject
+@end
+@implementation PodsDummy_Pods_NYTimesNewsTests
+@end
